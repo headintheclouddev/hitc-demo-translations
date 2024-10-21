848a0a9f2007a05d7c12e55008b1dac1e7bef0a2
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2024-10-21 13:20:06 +0100

    Add SuiteScript translation example

--- src/FileCabinet/SuiteScripts/hitc-demo-translations/productFeatureUserEvent.js
+++ src/FileCabinet/SuiteScripts/hitc-demo-translations/productFeatureUserEvent.js
@@ -5,11 +5,18 @@
  * @NScriptType UserEventScript
  * @NApiVersion 2.1
  */
-define(["require", "exports", "N/log"], function (require, exports, log) {
+define(["require", "exports", "N/log", "N/ui/message", "N/translation"], function (require, exports, log, message, translation) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.beforeLoad = beforeLoad;
     function beforeLoad(context) {
         log.debug('beforeLoad', `${context.type} product feature record ${context.newRecord.id}`);
+        if (context.type == context.UserEventType.VIEW || context.type == context.UserEventType.EDIT) {
+            context.form.addPageInitMessage({
+                type: message.Type.WARNING,
+                title: translation.get({ collection: 'custcollection_hitc_demo', key: 'record_out_of_date' })(),
+                message: translation.get({ collection: 'custcollection_hitc_demo', key: 'please_update' })(),
+            });
+        }
     }
 });
--- src/TypeScripts/productFeatureUserEvent.ts
+++ src/TypeScripts/productFeatureUserEvent.ts
@@ -8,7 +8,16 @@
 
 import {EntryPoints} from "N/types";
 import log = require('N/log');
+import message = require('N/ui/message');
+import translation = require('N/translation');
 
 export function beforeLoad(context: EntryPoints.UserEvent.beforeLoadContext) {
   log.debug('beforeLoad', `${context.type} product feature record ${context.newRecord.id}`);
+  if (context.type == context.UserEventType.VIEW || context.type == context.UserEventType.EDIT) {
+    context.form.addPageInitMessage({
+      type:    message.Type.WARNING,
+      title:   translation.get({ collection: 'custcollection_hitc_demo', key: 'record_out_of_date' })(),
+      message: translation.get({ collection: 'custcollection_hitc_demo', key: 'please_update'      })(),
+    });
+  }
 }
