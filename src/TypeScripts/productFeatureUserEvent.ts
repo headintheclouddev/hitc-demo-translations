3104a79988ae0be7c4836120f584e73957444ba5
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2024-10-24 14:51:13 +0100

    User Event: Reset to starting point

--- src/TypeScripts/productFeatureUserEvent.ts
+++ src/TypeScripts/productFeatureUserEvent.ts
@@ -9,15 +9,12 @@
 import {EntryPoints} from "N/types";
 import log = require('N/log');
 import message = require('N/ui/message');
-import translation = require('N/translation');
 
 export function beforeLoad(context: EntryPoints.UserEvent.beforeLoadContext) {
   log.debug('beforeLoad', `${context.type} product feature record ${context.newRecord.id}`);
   if (context.type == context.UserEventType.VIEW || context.type == context.UserEventType.EDIT) {
-    context.form.addPageInitMessage({
-      type:    message.Type.WARNING,
-      title:   translation.get({ collection: 'custcollection_hitc_demo', key: 'record_out_of_date' })(),
-      message: translation.get({ collection: 'custcollection_hitc_demo', key: 'please_update'      })(),
-    });
+    const bannerTitle   = 'Record is out of date';
+    const bannerMessage = 'Please update the content of this product feature description';
+    context.form.addPageInitMessage({ type: message.Type.WARNING, title: bannerTitle, message: bannerMessage });
   }
 }
